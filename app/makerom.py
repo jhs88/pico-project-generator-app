dd79b3495f0148608af6fb19635598e677217b4f
Joseph Scherreik <scherreikj@gmail.com>
2024-01-06 20:33:24 -0500

    Add new files and configurations

--- app/makerom.py
+++ app/makerom.py
@@ -4,17 +4,17 @@
 #
 
 code = bytearray([
-  0xa9, 0xff,         # lda #$ff
-  0x8d, 0x02, 0x60,   # sta $6002
+    0xa9, 0xff,         # lda #$ff
+    0x8d, 0x02, 0x60,   # sta $6002
 
-  0xa9, 0x55,         # lda #$55
-  0x8d, 0x00, 0x60,   # sta $6000
+    0xa9, 0x55,         # lda #$55
+    0x8d, 0x00, 0x60,   # sta $6000
 
-  0xa9, 0xaa,         # lda #$aa
-  0x8d, 0x00, 0x60,   # sta $6000
+    0xa9, 0xaa,         # lda #$aa
+    0x8d, 0x00, 0x60,   # sta $6000
 
-  0x4c, 0x05, 0x80,   # jmp $8005
-  ])
+    0x4c, 0x05, 0x80,   # jmp $8005
+    ])
 
 rom = code + bytearray([0xea] * (32768 - len(code)))
 
@@ -23,5 +23,5 @@ rom[0x7ffd] = 0x80
 
 print("Building ROM\nWriting rom.bin...")
 with open("rom.bin", "wb") as out_file:
-  out_file.write(rom)
+    out_file.write(rom)
 print("Done.")
